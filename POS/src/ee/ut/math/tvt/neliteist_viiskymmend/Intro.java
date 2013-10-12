5097e5b3469360d66a8c285f84a39f00a2929b4a
Vitaos <pakisupp@msn.com>
2013-10-12 03:16:05 +0300

    system updates

--- POS/src/ee/ut/math/tvt/neliteist_viiskymmend/Intro.java
+++ POS/src/ee/ut/math/tvt/neliteist_viiskymmend/Intro.java
@@ -1,10 +1,15 @@
 package ee.ut.math.tvt.neliteist_viiskymmend;
 
+import java.io.IOException;
 
 public class Intro {
         
-        public static void main(String[] args) {
-
+        public static void main(String[] args) throws IOException {
+        	
+        	IntroUI introUI = new IntroUI("application.properties","version.properties");
+        	introUI.setVisible(true);
+            introUI.setAlwaysOnTop(true);
+            
         }
 
 }
--- POS/src/ee/ut/math/tvt/neliteist_viiskymmend/IntroUI.java
+++ POS/src/ee/ut/math/tvt/neliteist_viiskymmend/IntroUI.java
@@ -1,7 +1,48 @@
 package ee.ut.math.tvt.neliteist_viiskymmend;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import java.io.*;
+import java.util.Properties;
+
+import javax.swing.*;
 
 public class IntroUI extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public IntroUI(String applicationProperties, String versionProperties) throws IOException {
+		
+		super("Team information");
+		
+		Properties properties = new Properties();
+		
+    	try {
 
+    		properties.load(new FileInputStream(applicationProperties));
+    		properties.load(new FileInputStream(versionProperties));
+
+    	} catch (IOException ex) {
+    		
+    		ex.printStackTrace();
+    		
+        }
+		
+    	JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    	JLabel info = new JLabel("<html><table border='0' width='100%'><tr><td><b>Team name:</b></td><td>" + properties.getProperty("team_name") + 
+    			"</td></tr><tr><td>Team leader:</td><td>" + properties.getProperty("team_leader") + 
+    			"</td></tr><tr><td>Team leader e-mail:</td><td>" + properties.getProperty("team_leader_email") + 
+    			"</td></tr><tr><td>Team members:</td><td>" + properties.getProperty("team_members") + 
+    			"</td></tr><tr><td>Team logo:</td><td><img border='0' src='" + properties.getProperty("team_logo") + "' width='150' height='150'>" +
+    			"</td></tr><tr><td>Software version number:</td><td>" + properties.getProperty("build.number") + 
+    			"</td></tr></table></html>");
+    	panel.add(info);
+    	
+    	add(panel);
+    	setSize(400, 320);
+    	setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    	
+    	
+	}
+	
 }
