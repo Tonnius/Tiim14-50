e8348583beb64931624a79ebadcfb76629f4c98a
Kersti Oja <kerstioja@windowslive.com>
2013-10-14 13:33:28 +0300

    logging

--- POS/src/ee/ut/math/tvt/neliteist_viiskymmend/IntroUI.java
+++ POS/src/ee/ut/math/tvt/neliteist_viiskymmend/IntroUI.java
@@ -3,12 +3,19 @@ package ee.ut.math.tvt.neliteist_viiskymmend;
 import java.awt.*;
 import java.io.*;
 import java.util.Properties;
+import java.util.Date;
 
 import javax.swing.*;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 
 public class IntroUI extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger log = Logger.getLogger(IntroUI.class);
 	
 	public IntroUI(String applicationProperties, String versionProperties) throws IOException {
 		
@@ -16,13 +23,15 @@ public class IntroUI extends JFrame {
 		
 		Properties properties = new Properties();
 		
+		BasicConfigurator.configure();
+		
     	try {
 
     		properties.load(new FileInputStream(applicationProperties));
     		properties.load(new FileInputStream(versionProperties));
 
     	} catch (IOException ex) {
-    		
+    		log.warn("Properties file not found!");
     		ex.printStackTrace();
     		
         }
@@ -33,7 +42,7 @@ public class IntroUI extends JFrame {
     			"</td></tr><tr><td>Team leader e-mail:</td><td>" + properties.getProperty("team_leader_email") + 
     			"</td></tr><tr><td>Team members:</td><td>" + properties.getProperty("team_members") + 
     			"</td></tr><tr><td>Team logo:</td><td><img border='0' src='" + properties.getProperty("team_logo") + "' width='150' height='150'>" +
-    			"</td></tr><tr><td>Software version number:</td><td>" + properties.getProperty("build.number") + 
+    			"</td></tr><tr><td>Software version number:</td><td>" + properties.getProperty("build.number") +
     			"</td></tr></table></html>");
     	
     	panel.add(information);
@@ -42,6 +51,10 @@ public class IntroUI extends JFrame {
     	setSize(400, 320);
     	setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        String date = new Date().toString();
+        
+        log.info("IntroUI opened " + date);
     	
     	
 	}
