381f6c04bfde0c9123c3c5dce64ecffcff2b415a
Toivo Alp <toitsvaiper@hotmail.com>
2013-10-28 23:43:36 +0200

    Toivo muudatus

---
+++ POS/src/ee/ut/math/tvt/salessystem/ui/panels/ConfirmOrderPanel.java
@@ -0,0 +1,121 @@
+package ee.ut.math.tvt.salessystem.ui.panels;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.NoSuchElementException;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.apache.log4j.Logger;
+
+import ee.ut.math.tvt.neliteist_viiskymmend.Intro;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
+import ee.ut.math.tvt.salessystem.ui.popups.ErrorPopup;
+import ee.ut.math.tvt.salessystem.ui.model.PurchaseInfoTableModel;
+
+public class ConfirmOrderPanel extends JPanel {
+
+	private SalesSystemModel model;
+
+	private static JTextField sumField;
+	private static JTextField paymentField;
+	private static JTextField changeField;
+	double orderSum = 0;
+	static boolean returnValue;
+
+	public static boolean isReturnValue() {
+		return returnValue;
+	}
+
+	public ConfirmOrderPanel(final SalesSystemModel model,
+			final JDialog popupDialog) {
+		this.model = model;
+		setLayout(new GridLayout(4, 2));
+		setBorder(BorderFactory.createTitledBorder("Confirm order: "));
+
+		// generate text fields
+		sumField = new JTextField();
+		paymentField = new JTextField();
+		changeField = new JTextField();
+
+		// button for confirming order
+		JButton confirmOrderButton = new JButton("Confirm");
+		confirmOrderButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if (orderSum > Double.parseDouble(paymentField.getText())) {
+						ErrorPopup
+								.createPopup("The amount payed is less than the charged amount!");
+					} else {
+						returnValue = true;
+						popupDialog.dispose();
+					}
+
+				} catch (NumberFormatException ex) {
+					ErrorPopup
+							.createPopup("Invalid payment data, order not processed!");
+					paymentField.setText("");
+
+				}
+
+			}
+		});
+
+		// button for closing window / canceling order confirmation
+		JButton closeButton = new JButton("Cancel");
+		closeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				returnValue = false;
+				popupDialog.dispose();
+			}
+		});
+
+		// calculate change amount when paymentField loses focus
+		paymentField.addFocusListener(new FocusListener() {
+			public void focusLost(FocusEvent e) {
+				changeField.setText(Double.toString(Math.round((Double
+						.parseDouble(paymentField.getText()) - orderSum) * 100) / 100));
+			}
+
+			public void focusGained(FocusEvent e) {
+			}
+		});
+
+		// set value for the total sum of the order
+		for (int i = 0; i < model.getCurrentPurchaseTableModel().getTableRows()
+				.size(); i++) {
+			orderSum += model.getCurrentPurchaseTableModel().getTableRows()
+					.get(i).getPrice()
+					* model.getCurrentPurchaseTableModel().getTableRows()
+							.get(i).getQuantity();
+		}
+		sumField.setText(Double.toString(Math.round(orderSum * 100) / 100));
+		changeField.setText("");
+		// add fields to panel
+		add(new JLabel("Order sum:"));
+		add(sumField);
+
+		add(new JLabel("Payment:"));
+		add(paymentField);
+
+		add(new JLabel("Change:"));
+		add(changeField);
+
+		add(confirmOrderButton);
+		add(closeButton);
+
+		sumField.setEditable(false);
+		changeField.setEditable(false);
+
+	}
+
+}
